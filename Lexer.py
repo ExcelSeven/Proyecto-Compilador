4abd2da244617a12242adc7307decb0d444818d6
Eli Moreira <coincidir777@hotmail.com>
2021-10-25 22:26:51 -0500

    Polished Lexer

--- Lexer.py
+++ Lexer.py
@@ -1,67 +1,109 @@
 import ply.lex as lex   # Import PLY lex module
-tokens = ('PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'EQUALS', 'COMPARE', 'AND', 'OR', 'LESSER', 'GREATER', 'LPAREN', 'RPAREN', 'LCURLY', 'RCURLY', 'LBRACK', 'RBRACK', 'SEMI', 'COLON', 'COMMA', 'QUOTE', 
-           'NAME', 'MESSAGE', 'NUMBER', 'PROGRAM', 'MAIN', 'VARS', 'INT', 'FLOAT', 'CHAR', 'VOID', 'FUNCTION', 'RETURN', 'READ', 'WRITE', 'IF', 'THEN', 'ELSE', 'WHILE', 'DO', 'FOR', 'TO')
+
+# List of reserved words
+reserved = {
+    'PROGRAM'   : 'PROGRAM', 
+    'MAIN'      : 'MAIN', 
+    'VARS'      : 'VARS', 
+    'INT'       : 'INT', 
+    'FLOAT'     : 'FLOAT', 
+    'CHAR'      : 'CHAR', 
+    'VOID'      : 'VOID', 
+    'FUNCTION'  : 'FUNCTION', 
+    'RETURN'    : 'RETURN', 
+    'READ'      : 'READ', 
+    'WRITE'     : 'WRITE', 
+    'IF'        : 'IF', 
+    'THEN'      : 'THEN', 
+    'ELSE'      : 'ELSE', 
+    'WHILE'     : 'WHILE', 
+    'DO'        : 'DO', 
+    'FOR'       : 'FOR', 
+    'TO'        : 'TO'
+}
+
+# List of tokens
+tokens = ['PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'EQUALS', 'COMPARE', 'AND', 'OR', 'LESSER', 'GREATER', 'LPAREN', 'RPAREN', 'LCURLY', 
+         'RCURLY', 'LBRACK', 'RBRACK', 'SEMI', 'COLON', 'COMMA', 'QUOTE', 'LESSER_EQ', 'GREATER_EQ', 'ID', 'C_INT', 'C_FLOAT', 'C_CHAR', 'MESSAGE']
+
+tokens = tokens + list(reserved.values())
 
 # Token definitions 
-t_PLUS      = r'\+'
-t_MINUS     = r'\-'
-t_TIMES     = r'\*'
-t_DIVIDE    = r'\/'
-t_EQUALS    = r'\='
-t_COMPARE   = r"=="
-t_AND       = r'\&'
-t_OR        = r'\|'
-t_LESSER    = r'\<'
-t_GREATER   = r'\>'
-t_LPAREN    = r'\('
-t_RPAREN    = r'\)'
-t_LCURLY    = r'\{'
-t_RCURLY    = r'\}'
-t_LBRACK    = r'\['
-t_RBRACK    = r'\]'
-t_SEMI      = r'\;'
-t_COLON     = r'\:'
-t_COMMA     = r'\,'
-t_QUOTE     = r'\"'
-t_NAME      = r'[a-zA-Z_][a-zA-Z0-9_]*'
-t_MESSAGE   = r'[a-zA-Z0-9_]*'
-t_PROGRAM   = r"Program"
-t_MAIN      = r"main"
-t_VARS      = r"VARS"
-t_INT       = r"int"
-t_FLOAT     = r"float"
-t_CHAR      = r"char"
-t_VOID      = r"void"
-t_FUNCTION  = r"function"
-t_RETURN    = r"return"
-t_READ      = r"read"
-t_WRITE     = r"write"
-t_IF        = r"if"
-t_THEN      = r"then"
-t_ELSE      = r"else"
-t_WHILE     = r"while"
-t_DO        = r"do"
-t_FOR       = r"for"
-t_TO        = r"to"
+t_PLUS       = r'\+'
+t_MINUS      = r'\-'
+t_TIMES      = r'\*'
+t_DIVIDE     = r'\/'
+t_EQUALS     = r'='
+t_COMPARE    = r'=='
+t_AND        = r'\&'
+t_OR         = r'\|'
+t_LESSER     = r'<'
+t_GREATER    = r'>'
+t_LPAREN     = r'\('
+t_RPAREN     = r'\)'
+t_LCURLY     = r'\{'
+t_RCURLY     = r'\}'
+t_LBRACK     = r'\['
+t_RBRACK     = r'\]'
+t_SEMI       = r'\;'
+t_COLON      = r'\:'
+t_COMMA      = r'\,'
+t_QUOTE      = r'\"'
+t_LESSER_EQ  = r'<='
+t_GREATER_EQ = r'>='
 
-def t_NUMBER(t):
-    r'[0-9]+'
-    try:
-        t.value = int(t.value)
-    except ValueError:
-        print("Integer value too large %d", t.value)
-        t.value = 0
+# Regular expressions with action code
+def t_ID(t):
+     r'[a-zA-Z_][a-zA-Z_0-9]+'
+     t.type = reserved.get(t.value,'ID')    # Check for reserved words
+     return t
+
+def t_C_FLOAT(t):
+    r'[0-9]+\.[0-9]+'
     return t
 
-# Ignored characters
+def t_C_INT(t):
+    r'[0-9]+'
+    return t
+
+def t_C_CHAR(t):
+    r'[a-zA-Z]'
+    return t
+
+def t_MESSAGE(t):
+    r'["][^\n]+["]'
+    return t
+
+# String containing ignored characters
 t_ignore    = ' \t'
 
+# Rule to track line numbers
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
+# Error handling rule
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-lex.lex()
+# Building the lexer
+lexer = lex.lex()
+
+# Test it out
+data = '''
+ 3 + 4 * 10
+   + -20.3
+   word = "hello haha anyway 328763872"
+   character = a
+ '''
+ 
+# Give the lexer some input
+lexer.input(data)
+ 
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok: 
+        break      # No more input
+    print(tok)
